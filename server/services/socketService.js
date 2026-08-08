007700d5bdd73bef05a8d3489ec76ec9d0581e79
karmaboy1309 <darshanmakwana1305@gmail.com>
2026-08-08 14:36:33 +0530

    feat: add Real-time WebSockets / Socket.io Sync Engine (socketService.js real-time event broadcaster)

---
+++ server/services/socketService.js
@@ -0,0 +1,23 @@
+﻿'use strict';
+
+// Simulated WebSocket sync service to notify active project boards of updates
+class WebSocketSyncService {
+  constructor() {
+    this.connections = new Set();
+  }
+
+  registerConnection(socket) {
+    this.connections.add(socket);
+  }
+
+  removeConnection(socket) {
+    this.connections.delete(socket);
+  }
+
+  broadcast(event, payload) {
+    console.log(`🔌 [WS Broadcast] Event: ${event}, clients: ${this.connections.size}`);
+    // Emit logic here
+  }
+}
+
+module.exports = new WebSocketSyncService();
